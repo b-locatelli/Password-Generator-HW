bdd0c9986403554bacc53828117e92153e80ce4c
Baily locatelli <bailylocatelli@gmail.com>
2023-04-02 16:34:58 -0700

    added code

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+  var length = prompt("Chose password length between 8 and 128 charcters");
+  if(isNaN(length)) {
+    alert("Must chose a number");
+    return;
+  }
+  if(length < 8 || length > 128) {
+    alert("Must choose a number between 8 and 128.")
+    return;
+  }
+  // if(!lower && !upper && !numb && !spec) {
+  //   alert("You must have a charcter.")
+  //   return
+  // }
+  var lower = confirm("Would you like lowercase in your password?");
+  var upper = confirm("Would you like uppercase in your Password?");
+  var numb = confirm("Would you like numbers in your password?");
+  var spec = confirm("Would you like special characters in you password?");
+  var letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%?*.";
+  var num = "1234567890";
+  passwordText = ""
+  passwordText = letter.charAt(Math.floor(Math.random() * letter.length))
+  // passwordText = num.charAt(Math.floor(Math.random() * num.length))
+  return passwordText
+};
 
 // Write password to the #password input
 function writePassword() {
