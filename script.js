5d99791aae5e847f42d78785f1be3219399b8d5d
Baily locatelli <bailylocatelli@gmail.com>
2023-04-02 17:29:03 -0700

    added comments

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// function for generated passowrd
 function generatePassword(){
+  // chose character amount
   var length = prompt("Chose password length between 8 and 128 charcters");
+  // makes sure you pick a number no strings
   if(isNaN(length)) {
     alert("Must chose a number");
     return;
   }
 
+  // Declines numbers over 128 and under 8
   if(length < 8 || length > 128) {
     alert("Must choose a number between 8 and 128.")
     return;
   }
 
+  // asks the user what characters they want in their password
   var lower = confirm("Would you like lowercase in your password?");
   var upper = confirm("Would you like uppercase in your Password?");
   var numb = confirm("Would you like numbers in your password?");
   var spec = confirm("Would you like special characters in you password?");
 
+  // makes sure atleast one character is chosen
   if(!lower && !upper && !numb && !spec) {
     alert("You must have atleast 1 character type.")
     return
   }
-  
+
+  // my variables
   var lowerLet = "abcdefghijklmnopqrstuvwxyz";
   var upperLet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var specLet = "!@$%?*."
@@ -29,6 +37,7 @@ function generatePassword(){
   var conf = ""
   passwordText = ""
 
+  // combines variables so all characters chosen can be used in the generated password
   if(lower) {
     conf += lowerLet
   }
